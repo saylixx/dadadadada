4ab7e31cd801ced163c3da961f19f0966c44e969
saylixx <81294849+saylixx@users.noreply.github.com>
2021-03-24 15:22:22 +0600

    Add files via upload

---
+++ ilyas.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>
+	 Ilyasbek Sydykov	
+	</title>
+	<p>Sydykov Ilyasbek</p>
+	<p>University : Ala-Too</p>
+	<p>Age : 17</p>
+	<p>Sex : Male </p>
+</head>
+<body>
+
+
+</body>
+</html>
