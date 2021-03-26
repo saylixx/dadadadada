8122c466f8daa7ea1fd238807836a7aa8cc8f2ef
saylixx <81294849+saylixx@users.noreply.github.com>
2021-03-26 10:07:38 +0600

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,10 @@
 	<p>Sydykov Ilyasbek</p>
 	<p>University : Ala-Too</p>
 	<p>Age : 17</p>
-	<p>Sex : Male </p>
+	<p>Gender : Male </p>
 </head>
 <body>
+	<img src="d23c5339-b0d7-47d5-ba74-71de03acef61.jpg"/>
 
 
 </body>
